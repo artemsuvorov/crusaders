474c9b5335489f3399b13179115dc8885963991f
Artem Suvorov <suvorov.artem@hotmail.com>
2023-05-16 12:33:33 +0500

    add unit taking damage by pressing P key

---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject highlight;
+
+    [SerializeField, Range(1.0f, 10.0f)]
+    private float speed = 1.0f;
+
+    private Vector3 targetPosition;
+
+    public void MoveTo(Vector3 position)
+    {
+        targetPosition = position;
+    }
+
+    public void Select()
+    {
+        highlight.SetActive(true);
+    }
+
+    public void Deselect()
+    {
+        highlight.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        targetPosition = transform.position;
+        highlight = transform.Find("Highlight").gameObject;
+        Deselect();
+    }
+
+    private void Update()
+    {
+        var destination = Vector2.MoveTowards(
+            transform.position,
+            targetPosition,
+            speed * Time.deltaTime
+            );
+
+        transform.position = destination;
+    }
+}
