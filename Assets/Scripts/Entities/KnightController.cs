766c10b1455c0953164916ef8862fe86cb6f51b7
Artem Suvorov <suvorov.artem@hotmail.com>
2023-06-19 12:40:00 +0500

    balance fixes

--- Assets/Scripts/Entities/KnightController.cs
+++ Assets/Scripts/Entities/KnightController.cs
@@ -2,9 +2,9 @@
 
 public class KnightController : UnitController
 {
-    private readonly Health health = new(80.0f);
+    private readonly Health health = new(100.0f);
 
-    public override Cost Cost => new(gold: 100);
+    public override Cost Cost => new(gold: 90);
     protected override Health Health => health;
     public override float Damage { get; protected set; } = 20.0f;
 }
--- Assets/Scripts/Entities/WorkerController.cs
+++ Assets/Scripts/Entities/WorkerController.cs
@@ -4,7 +4,7 @@ public class WorkerController : UnitController
 {
     private readonly Health health = new(30.0f);
 
-    public override Cost Cost => new(gold: 40);
+    public override Cost Cost => new(gold: 30);
     protected override Health Health => health;
     public override float Damage { get; protected set; } = 10.0f;
 }
